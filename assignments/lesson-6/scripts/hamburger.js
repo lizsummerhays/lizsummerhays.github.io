a4feeb8bf2f85638b421a3b9868138e4e6f69ff1
lizsummerhays <lizsummerhays@gmail.com>
2019-06-01 15:43:04 -0600

    update

--- assignments/lesson-6/scripts/hamburger.js
+++ assignments/lesson-6/scripts/hamburger.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 function toggleMenu() {
     document.getElementsByClassName("navigation")
     [0].classList.toggle("responsive");
 }
-=======
-function toggleMenu() {
-    document.getElementsByClassName("navigation")
-    [0].classList.toggle("responsive");
-}
->>>>>>> e10d4e8147e485f98997032ba4f6706bc9ba3c34
