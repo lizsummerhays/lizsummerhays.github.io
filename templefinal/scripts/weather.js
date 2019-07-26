06bd3e95eef07cf8e74e0fe7e267f42503e3a636
lizsummerhays <lizsummerhays@gmail.com>
2019-07-25 23:29:18 -0600

    update

---
+++ templefinal/scripts/weather.js
@@ -0,0 +1,17 @@
+let weatherRequest = new XMLHttpRequest();
+let apiURLstring = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=bf1238b45835dd1dadd9096d7f55cc79";
+weatherRequest.open('Get', apiURLstring, true);
+weatherRequest.send();
+
+weatherRequest.onload = function() {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+
+    document.getElementById('cc-temp').innerHTML = weatherData.main.temp;
+    document.getElementById('high-temp').innerHTML = weatherData.main.temp_max;
+    document.getElementById('low-temp').innerHTML = weatherData.main.temp_min;
+    document.getElementById('cc-humidity').innerHTML = weatherData.main.humidity;
+    document.getElementById('cc-speed').innerHTML = weatherData.wind.speed;
+
+}
+
